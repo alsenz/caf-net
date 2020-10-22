45a7ab1bc48b2237971845a7e084254e073fcff7
Tom Plowman <tom.plowman@assured.ai>
2020-10-22 10:58:56 +0100

    adding new convention

--- src/net-typeids.hpp
+++ src/net-typeids.hpp
@@ -8,6 +8,7 @@ namespace as::net {
     // Block out a silly custom type block
     constexpr caf::type_id_t custom_type_block_start = 18100; //Some sufficiently large far-away type block
     constexpr caf::type_id_t custom_type_block_end = custom_type_block_start + 32; //Leaves us 32 custom types
+    constexpr caf::type_id_t custom_type_block_safe_max = custom_type_block_start + 999; // We promsie not to use more than a thosuand typeids.
 
     // Lots of forward declarations
     class request;
