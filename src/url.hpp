b2a62c7216a82324f50701193ebde61375c16cac
Tom Plowman <tom.plowman@assured.ai>
2020-11-28 12:54:45 +0000

    making url hashable

--- src/url.hpp
+++ src/url.hpp
@@ -1,7 +1,24 @@
+#include <functional>
+
 #include "url.h"
+
+#include "common/hash-combine.hpp"
 
 namespace gnt::dln {
 
     using url = Url::Url;
 
 } //ns gnt::dln
+
+namespace std {
+
+    template<>
+    struct hash<gnt::dln::url> {
+        std::size_t operator()(const gnt::dln::url &x) const {
+            std::size_t seed = x.port()+7;
+            hash_combine(seed, x.host(), x.path(), x.params(), x.query(), x.fragment(), x.userinfo());
+            return seed;
+        }
+    };
+
+} //ns std
